bedb9f54a06422da4ded57fce5c72e9661f428f3
muziqianjunxiu <muziqianjunxiu@outlook.com>
2019-04-07 11:55:13 +0800

    栈的链式存储定义

--- Stack/LinkStackDefine.py
+++ Stack/LinkStackDefine.py
@@ -9,7 +9,7 @@ class StackNode:
 #定义链栈类
 class LinkStack:
     ''' 
-    链栈的初始化：即初始化一个栈顶节点，其数据域为None，指针域next始终指向栈顶元素，是不移动的。
+    带头节点链栈的初始化：即初始化一个栈顶节点，其数据域为None，指针域next始终指向栈顶元素，是不移动的。
     元素入栈即：该指针next域改为指向该元素，而该元素next域接手指向self.top指向的入栈元素，即之前入栈的元素
     元素出栈即：该指针next域改为指向第二个元素
     而 顺序栈的初始化，其实就是初始化一个数组 s ，用来存放入栈元素，和一个标记 top ，用来指示栈顶位置。
@@ -39,15 +39,13 @@ class LinkStack:
         tStackNode.next=self.top.next
         self.top.next=tStackNode
         print('当前进栈元素为：',x)
-
 #获取栈顶元素
     def GetTopStack(self):
         if self.IsEmptyStack():
             print('栈为空')
             return
         else:
-            return self.top.next.data
-            
+            return self.top.next.data          
 #创建栈
     def CreateStackByInput(self):
         data=input('输入入栈元素，“#”键结束：')
@@ -55,7 +53,7 @@ class LinkStack:
             self.PushStack(data)
             data=input('输入入栈元素：')
         return
-#栈的遍历
+#栈的正序遍历
     def StackTraverse(self):
         if self.IsEmptyStack():
             print('栈为空')
@@ -80,7 +78,6 @@ class LinkStack:
             while itop.next!=None:
                 itop=itop.next
                 tlist.append(itop.data)
-               
             #print(tlist[::-1])
             print('反序输出栈为：')
             for i in reversed(tlist):
